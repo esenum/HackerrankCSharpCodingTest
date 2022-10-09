00ec2e30fdc1d52c9f64b9c796f1015fbb055ac3
uesen94 <75943806+uesen94@users.noreply.github.com>
2022-10-09 14:15:42 +0300

    First commit

---
+++ MyListFunctionBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace HackerrankCSharpCodingTest
+{
+    public static class MyListFunctionBase
+    {
+        public static void MySortFunction(this List<int> li)
+        {
+            int temp;
+            for (int i = 0; i < li.Count; i++)
+            {
+                for (int j = 0; j < li.Count; j++)
+                {
+                    if (li[i] < li[j])
+                    {
+                        temp = li[i];
+                        li[i] = li[j];
+                        li[j] = temp;
+                    }
+
+                }
+
+                Console.WriteLine(".......................");
+                foreach (var item in li)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine(".......................");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerrankCSharpCodingTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> li=new List<int>();
+            li.Add(1);
+            li.Add(4);
+            li.Add(3);
+            li.Add(2);
+            foreach(var item in li)
+            {
+                Console.WriteLine(item);
+            }
+            li.MySortFunction();
+            Console.WriteLine("After sorting");
+            foreach(var item in li)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        
+    }
+}
